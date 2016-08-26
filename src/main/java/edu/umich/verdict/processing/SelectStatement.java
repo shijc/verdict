81610c1d048dabe6082ca47c64088671f2afad75
shijc <shijc@umich.edu>
2016-08-26 16:30:24 -0400

    modify test scripts

--- src/main/java/edu/umich/verdict/processing/SelectStatement.java
+++ src/main/java/edu/umich/verdict/processing/SelectStatement.java
@@ -12,6 +12,8 @@ import edu.umich.verdict.transformation.TransformedQuery;
 import org.antlr.v4.runtime.TokenStreamRewriter;
 import org.antlr.v4.runtime.tree.ParseTree;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -24,21 +26,66 @@ public class SelectStatement extends ParsedStatement {
         this.rewriter = rewriter;
     }
 
+    // used for performance test
+    protected String getPerformanceStatement(String table) {
+        String sql = "select\n" +
+                "sum(extendedprice) as sum_price,\n" +
+                "avg(extendedprice) as avg_price\n" +
+                "from\n" +
+                table + "\n" +
+                "where\n" +
+                "shipdate <= '1996-01-01'\n" +
+                "and linestatus = 'F'";
+        return sql;
+    }
+
+    // used for running system call (e.g. to clean cache)
+    protected String executeCommand(String command) {
+
+        StringBuffer output = new StringBuffer();
+
+        Process p;
+        try {
+            p = Runtime.getRuntime().exec(command);
+            p.waitFor();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            String line = "";
+            while ((line = reader.readLine())!= null) {
+                output.append(line + "\n");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return output.toString();
+
+    }
+
     @Override
     public ResultSet run(Configuration conf, DbConnector connector) throws SQLException, InvalidConfigurationException, InvalidSyntaxException {
-        ResultSet rs;
+        ResultSet rs = connector.executeQuery("select 1");
         TransformedQuery transformed = QueryTransformer.forConfig(conf, connector.getMetaDataManager(), this).transform();
-        String q = transformed.toString();
+
+        String tablename = "expr_performance.lineitem_1";
+        String q = getPerformanceStatement(tablename);
         if (transformed.isChanged()) {
-//            info("New Query:");
-//            info(q);
-//            info("\n");
-            info("Using Sample: " + transformed.getSample().getName() + " Size: " + (transformed.getSample().getCompRatio() *
-                    100) + "%" + " Type: " + (transformed.getSample() instanceof StratifiedSample ? "Stratified" : "Uniform"));
-//            info("Bootstrap Trials: " + transformed.getBootstrapTrials());
-//            info("Method: " + transformed.getMethod());
-            rs = connector.executeQuery(q);
-            rs = new VResultSet(rs, transformed, conf);
+            info("Query:");
+            info(q);
+            info("\n");
+            System.out.println("Method: " + transformed.getMethod() + "Table: " + transformed.getSample());
+            rs = connector.executeQuery("uncache table " + tablename);
+
+            for (int i = 0; i < 11; i++) {
+                //executeCommand("/home/shijc/code/emptyCache/restartDB");
+
+                long startTime = System.nanoTime();
+                rs = connector.executeQuery(q);
+                long estimatedTime = System.nanoTime() - startTime;
+                System.out.println(estimatedTime / 1000000000.0);
+                rs = connector.executeQuery("cache table " + tablename);
+            }
         } else {
             info("Running the original query...");
             rs = connector.executeQuery(q);
