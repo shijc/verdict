efee36cdcad9ce2d03ffd45906f935efd170125f
shijc <shijc@umich.edu>
2016-08-29 02:17:23 -0400

    Include rescaling test transformation and some other test scripts

--- src/main/java/edu/umich/verdict/processing/SelectStatement.java
+++ src/main/java/edu/umich/verdict/processing/SelectStatement.java
@@ -39,6 +39,77 @@ public class SelectStatement extends ParsedStatement {
         return sql;
     }
 
+    protected String getRescalingStatement(Configuration conf, String tablename) {
+        int poisson = Integer.parseInt(conf.get("bootstrap.trials"));
+        String q = "select sum((extendedprice)*lineitem40.v__weight) AS sum_base_price, sum((extendedprice)*lineitem40.v__weight)/sum(lineitem40.v__weight) AS avg_price";
+
+        q = q + ", sum((t.AVG_Y2 + POW(t.SUM_P1*(1 - 1/lineitem40.v__weight)/(t.COUNT_ALL - 1),0.5)*(extendedprice - t.AVG_Y2)) *(lineitem40.v__weight * t.COUNT_ALL / t.SUM_P1)*v__p1)";
+
+        for (int i = 2; i <= poisson; i++) {
+            q = q + ", sum((t.AVG_Y2 + POW(t.SUM_P" + i + "*(1 - 1/lineitem40.v__weight)/(t.COUNT_ALL - 1),0.5)*(extendedprice - t.AVG_Y2)) *(lineitem40.v__weight * t.COUNT_ALL / t.SUM_P" + i + ")*v__p";
+            q = q + i;
+            q = q + ")";
+        }
+
+        q = q + ", sum((t.AVG_Y2 + POW(t.SUM_P1*(1 - 1/lineitem40.v__weight)/(t.COUNT_ALL - 1),0.5)*(extendedprice - t.AVG_Y2)) *(lineitem40.v__weight * t.COUNT_ALL / t.SUM_P1)*v__p1) / sum((lineitem40.v__weight * t.COUNT_ALL / t.SUM_P1)*v__p1)";
+
+        for (int i = 2; i <= poisson; i++) {
+            q = q + ", sum((t.AVG_Y2 + POW(t.SUM_P" + i + "*(1 - 1/lineitem40.v__weight)/(t.COUNT_ALL - 1),0.5)*(extendedprice - t.AVG_Y2)) *(lineitem40.v__weight * t.COUNT_ALL / t.SUM_P" + i + ")*v__p" + i + ")/ sum((lineitem40.v__weight * t.COUNT_ALL / t.SUM_P" + i + ")*v__p";
+            q = q + i;
+            q = q + ")";
+        }
+
+        q = q + " from " + tablename + " as lineitem40, (SELECT v__weight, AVG(extendedprice) AS AVG_Y2, COUNT(*) AS COUNT_ALL";
+
+        for (int i = 1; i <= poisson; i++) {
+            q = q + ", SUM(V__P" + i + ") AS SUM_P" + i;
+        }
+
+        q = q + " from " + tablename  + " as lineitem40 where shipdate <= '1996-01-01' and linestatus = 'F' group by v__weight) as t";
+
+        q = q + " where shipdate <= '1996-01-01' and linestatus = 'F' and t.v__weight = lineitem40.v__weight";
+
+        return q;
+
+    }
+
+    protected void runRescaling(Configuration conf, DbConnector connector, String tablesize, String percentage) throws SQLException, InvalidConfigurationException {
+        String tablename = "expr_performance.s_s" + tablesize + "_" + percentage;
+        String q;
+
+        System.out.println("------Rescaling " + tablesize + "G " + percentage + "%------");
+        q = getRescalingStatement(conf,tablename);
+        ResultSet rs = connector.executeQuery("uncache table `" + tablename + "`");
+        for (int i = 0; i < 6; i++) {
+            //executeCommand("/home/shijc/code/emptyCache/restartDB");
+            long startTime = System.nanoTime();
+            rs = connector.executeQuery(q);
+            long estimatedTime = System.nanoTime() - startTime;
+            System.out.println(estimatedTime / 1000000000.0);
+            rs = connector.executeQuery("cache table `" + tablename + "`");
+        }
+    }
+
+    protected void runTable(DbConnector connector, String tablename) throws SQLException, InvalidConfigurationException {
+
+        ResultSet rs = connector.executeQuery("uncache table `" + tablename + "`");
+
+        String q = "select sum(extendedprice) as sum_price, "
+                + "avg(extendedprice) as avg_price from "
+                + tablename
+                +" where shipdate <= '1996-01-01'\n"
+                + "and linestatus = 'F'";
+
+        for (int i = 0; i < 6; i++) {
+            long startTime = System.nanoTime();
+            rs = connector.executeQuery(q);
+            long estimatedTime = System.nanoTime() - startTime;
+            System.out.println(estimatedTime / 1000000000.0);
+            rs = connector.executeQuery("cache table `"
+                    + tablename + "`");
+        }
+    }
+
     // used for running system call (e.g. to clean cache)
     protected String executeCommand(String command) {
 
@@ -73,20 +144,58 @@ public class SelectStatement extends ParsedStatement {
         //String tablename = "default.lineitem40";
         //String q = getPerformanceStatement(tablename);
         if (transformed.isChanged()) {
-            
-            System.out.println("Method: " + transformed.getMethod() + "Table: " + tablename);
-            rs = connector.executeQuery("uncache table `" + tablename + "`");
 
-            for (int i = 0; i < 6; i++) {
-                //executeCommand("/home/shijc/code/emptyCache/restartDB");
-                
-                long startTime = System.nanoTime();
-                rs = connector.executeQuery(q);
-                long estimatedTime = System.nanoTime() - startTime;
-                System.out.println(estimatedTime / 1000000000.0);
-                //spark.cacheTable(tablename);
-                //rs = connector.executeQuery("uncache table `" + tablename + "`");
-                rs = connector.executeQuery("cache table `" + tablename + "`");
+            runRescaling(conf,connector,"1","01");
+            runRescaling(conf,connector,"10","01");
+            runRescaling(conf,connector,"30","01");
+
+            System.out.println("--- Run 0.1% ---");
+            runTable(connector, "expr_performance.l1_01");
+            runTable(connector, "expr_performance.l10_01");
+            runTable(connector, "expr_performance.l30_01");
+
+            System.out.println("--- Run 1% ---");
+            runTable(connector, "expr_performance.l1_1");
+            runTable(connector, "expr_performance.l10_1");
+            runTable(connector, "expr_performance.l30_1");
+
+            System.out.println("--- Run 2% ---");
+            runTable(connector, "expr_performance.l1_2");
+            runTable(connector, "expr_performance.l10_2");
+            runTable(connector, "expr_performance.l30_2");
+
+            System.out.println("--- Run 5% ---");
+            runTable(connector, "expr_performance.l1_5");
+            runTable(connector, "expr_performance.l10_5");
+            runTable(connector, "expr_performance.l30_5");
+
+            System.out.println("--- Run original datasets ---");
+            runTable(connector, "expr_performance.lineitem_1");
+            runTable(connector, "expr_performance.lineitem_10");
+            runTable(connector, "default.lineitem40");
+
+
+
+
+
+
+
+
+
+
+            runRescaling(conf,connector,"1","1");
+            runRescaling(conf,connector,"10","1");
+            runRescaling(conf,connector,"30","1");
+
+            runRescaling(conf,connector,"1","2");
+            runRescaling(conf,connector,"10","2");
+            runRescaling(conf,connector,"30","2");
+
+            runRescaling(conf,connector,"1","5");
+            runRescaling(conf,connector,"10","5");
+            runRescaling(conf,connector,"30","5");
+
+
             }
         } else {
             info("Running the original query...");
